478a4c0af590ca24a773d51055b47864ed1a2181
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-24 19:17:47 +0200

    +chatForm submit event listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 const socket = io();
+const chatForm = document.getElementById('chat-form')
 
 socket.on('message', message => {
     console.log(message)
+})
+
+chatForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+let msg = e.target.elements.message.value
+console.log(msg);
 })
