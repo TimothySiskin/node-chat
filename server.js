8e8bdb8b47809a3461c93f2cd21440a5fd06afd7
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-24 09:01:06 +0200

    added socket.io and created io.on connection

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
 
 //creating express server
 const app = express();
+const server = http.createServer(app)
+
+//Socet io
+io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+//on client connection
+
+io.on("connection", socket => {
+console.log("client connected")
+})
+
 //use port 3000 unless there exist a preconfigured port
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{console.log(`server running on ${PORT}`)});
+server.listen(PORT, ()=>{console.log(`server running on ${PORT}`)});
 
