a4070bddf12344ca2092ad42186d22ae616c2a73
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-25 09:36:36 +0200

    emiting message to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 const socket = io();
 const chatForm = document.getElementById('chat-form')
 
+
+//Message from server
 socket.on('message', message => {
     console.log(message)
+    outputMessage(message)
 })
 
 chatForm.addEventListener('submit', (e)=>{
@@ -14,3 +17,13 @@ let msg = e.target.elements.message.value
 //Emit msg to server
 socket.emit('chat-message', msg);
 })
+
+//output message to DOM
+
+function outputMessage(message){
+const div = document.createElement('div');
+div.innerHTML = `<p class="meta">Placholder meta</p>
+<p class="text">${message}</p>`
+div.classList.add('message')
+document.querySelector('.chat-messages').appendChild(div)
+}
