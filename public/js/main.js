49875558c39f9ba613c0c40288cbc4e5b16b7530
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-27 15:42:55 +0200

    used prettier to format code

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,37 @@
 const socket = io();
-const chatForm = document.getElementById('chat-form')
-const chatMessages = document.querySelector('.chat-messages')
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
 
 //Message from server
-socket.on('message', message => {
-    console.log(message)
-    outputMessage(message)
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
 
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
-    //scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-})
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-chatForm.addEventListener('submit', (e)=>{
-e.preventDefault();
+  //Get msg text
+  let msg = e.target.elements.message.value;
 
-//Get msg text
-let msg = e.target.elements.message.value
+  //Emit msg to server
+  socket.emit("chat-message", msg);
 
-//Emit msg to server
-socket.emit('chat-message', msg);
-
-
-//Clear input
-e.target.elements.message.value = '';
-e.target.elements.message.focus();
-
-
-})
-
-
+  //Clear input
+  e.target.elements.message.value = "";
+  e.target.elements.message.focus();
+});
 
 //output message to DOM
 
-function outputMessage(message){
-    const {username, text, time} = message;
-const div = document.createElement('div');
-div.innerHTML = `<p class="meta">${username} <span>${time}</span></p>
-<p class="text">${text}</p>`
-div.classList.add('message')
-document.querySelector('.chat-messages').appendChild(div)
+function outputMessage(message) {
+  const { username, text, time } = message;
+  const div = document.createElement("div");
+  div.innerHTML = `<p class="meta">${username} <span>${time}</span></p>
+<p class="text">${text}</p>`;
+  div.classList.add("message");
+  document.querySelector(".chat-messages").appendChild(div);
 }
--- server.js
+++ server.js
@@ -1,51 +1,49 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io')
-const formatMessages = require('./utils/messages')
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessages = require("./utils/messages");
 
 //creating express server
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 //Socet io
 io = socketio(server);
 
 //set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'chatBot';
+const botName = "chatBot";
 
 //on client connection
 
-io.on("connection", socket => {
-console.log("client connected")
-socket.emit('message', formatMessages(botName,"Welcome!"))
+io.on("connection", (socket) => {
+  console.log("client connected");
+  socket.emit("message", formatMessages(botName, "Welcome!"));
 
-//Broadcast when a user connets
+  //Broadcast when a user connets
 
-socket.broadcast.emit('message', formatMessages('botName','A user has join the chat'))
+  socket.broadcast.emit(
+    "message",
+    formatMessages("botName", "A user has join the chat")
+  );
 
-//on disconnetion
+  //on disconnetion
 
-socket.on('disconnect', ()=>{
-io.emit('message', formatMessages('botName','A User has left the chat'))
-})
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessages("botName", "A User has left the chat"));
+  });
 
-
-//Listen for msg
-socket.on('chat-message', (msg) => {
-    io.emit('message',formatMessages('USER', msg))
-    })
-
-
-})
-
-
-
+  //Listen for msg
+  socket.on("chat-message", (msg) => {
+    io.emit("message", formatMessages("USER", msg));
+  });
+});
 
 //use port 3000 unless there exist a preconfigured port
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, ()=>{console.log(`server running on ${PORT}`)});
-
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
