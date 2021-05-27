1b4fa5f1ab18e33abe1120181f813e76d42544c3
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-05-27 20:32:17 +0200

    fixed getRoomUsers function

--- utils/users.js
+++ utils/users.js
@@ -24,11 +24,17 @@ function userLeaves(id) {
   const index = users.findIndex((user) => user.id === id);
 
   if (index !== -1) {
-    return users.splice(index, 1);
+    return users.splice(index, 1)[0];
   }
+}
+
+function getRoomUsers(room) {
+  return users.filter((user) => user.room === room);
 }
 
 module.exports = {
   userJoin,
   getCurrentUser,
+  userLeaves,
+  getRoomUsers,
 };
