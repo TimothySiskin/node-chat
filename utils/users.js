6e3235d2e9714716ac855b7d6e78a71ffd2be13a
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-27 18:35:11 +0200

    +userLeaves

--- utils/users.js
+++ utils/users.js
@@ -20,6 +20,14 @@ function getCurrentUser(id) {
   return users.find((user) => user.id === id);
 }
 
+function userLeaves(id) {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1);
+  }
+}
+
 module.exports = {
   userJoin,
   getCurrentUser,
