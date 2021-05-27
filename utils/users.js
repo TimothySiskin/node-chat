55346c15752360ca68d29f4990931b6f69ff18fe
TimothySiskin <tymoteuszczyz@gmail.com>
2021-05-27 16:53:36 +0200

    +userJoin() +getCurrentUser()

---
+++ utils/users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+//Join users to chat
+
+function userJoin(id, username, room) {
+  const user = {
+    id,
+    username,
+    room,
+  };
+
+  users.push(user);
+
+  return user;
+}
+
+//Get curren user
+
+function getCurrentUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+};
